91036c0c7b15e657df9f20f8699d73ff50e278bc
Goshido <GoshidoMatazuki@mail.ru>
2017-03-05 22:09:38 +0300

    GXPhysics

---
+++ Include/GXPhysics/GXCollisionData.h
@@ -0,0 +1,18 @@
+//version 1.0
+
+#ifndef GX_COLLISION_DATA
+#define GX_COLLISION_DATA
+
+
+#include "GXContact.h"
+
+
+class GXCollisionData
+{
+	private:
+		GXContact*		contacts;
+		GXUInt			contactLeft;
+};
+
+
+#endif GX_COLLISION_DATA
---
+++ Include/GXPhysics/GXContact.h
@@ -0,0 +1,19 @@
+//version 1.0
+
+#ifndef GX_CONTACT
+#define GX_CONTACT
+
+
+#include <GXCommon/GXMath.h>
+
+
+class GXContact
+{
+	private:
+		GXVec3		point;
+		GXVec3		normal;
+		GXFloat		penetration;
+};
+
+
+#endif GX_CONTACT
